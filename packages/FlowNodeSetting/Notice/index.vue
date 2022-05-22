f96be24d1bd60a04d7cf18c5426abd84b9d63c54
luop <1950013600@qq.com>
2022-05-22 22:21:23 +0800

    wip: 通知节点配置信息优化

--- packages/FlowNodeSetting/Notice/index.vue
+++ packages/FlowNodeSetting/Notice/index.vue
@@ -16,66 +16,75 @@
       </span>
     </template>
     <div class="flow-setting-module">
-      <div class="flow-setting-content">
-        <div class="flow-setting-item">
-          <p class="flow-setting-item-title">节点名称</p>
-          <a-input v-model="node.nodeName" :size="size" class="w-full" placeholder="节点名称" />
-        </div>
-        <div class="flow-setting-item">
-          <p class="flow-setting-item-title">通知类型</p>
-          <a-checkbox-group v-model="noticeType">
-            <a-row :gutter="[24, 24]">
-              <a-col :span="8" v-for="(notice, i) in notices" :key="i">
-                <a-checkbox :value="notice.value">{{ notice.name }}</a-checkbox>
-              </a-col>
-            </a-row>
-          </a-checkbox-group>
-        </div>
-        <div class="flow-setting-item">
-          <p class="flow-setting-item-title">通知内容</p>
-          <div class="tpl-flex-box">
-            <div>选择已审核模板</div>
-            <a-button type="link" icon="plus-circle">
-              创建新模板
-            </a-button>
+      <a-tabs>
+        <a-tab-pane key="1" tab="通知设置">
+          <div class="flow-setting-content">
+            <div class="flow-setting-item">
+              <p class="flow-setting-item-title">节点名称</p>
+              <a-input v-model="node.nodeName" :size="size" class="w-full" placeholder="节点名称" />
+            </div>
+            <div class="flow-setting-item">
+              <p class="flow-setting-item-title">通知类型</p>
+              <a-checkbox-group v-model="noticeType">
+                <a-row :gutter="[24, 24]">
+                  <a-col :span="8" v-for="(notice, i) in notices" :key="i">
+                    <a-checkbox :value="notice.value">{{ notice.name }}</a-checkbox>
+                  </a-col>
+                </a-row>
+              </a-checkbox-group>
+            </div>
+            <div class="flow-setting-item">
+              <p class="flow-setting-item-title">发送通知人</p>
+              <FlowNodeApproval :groups="node.approverGroup" :node="node" title="通知人" />
+            </div>
+            <div v-if="noticeType.includes(2)" class="flow-setting-item">
+              <p class="flow-setting-item-title">外部手机号</p>
+              <a-button type="link" icon="plus" block>
+                添加手机号
+              </a-button>
+            </div>
+            <div v-if="noticeType.includes(3)" class="flow-setting-item">
+              <p class="flow-setting-item-title">外部邮箱账号</p>
+              <a-button type="link" icon="plus" block>
+                添加邮箱
+              </a-button>
+            </div>
+            <div v-if="noticeType.includes(3)" class="flow-setting-item">
+              <a-checkbox-group v-model="emailExt">
+                <a-row :gutter="[12, 12]">
+                  <a-col :span="12" v-for="(item, i) in emailItems" :key="i">
+                    <a-checkbox :value="item.value">{{ item.name }}</a-checkbox>
+                  </a-col>
+                </a-row>
+              </a-checkbox-group>
+            </div>
+            <div v-if="noticeType.includes(3) && emailExt.includes(1)" class="flow-setting-item">
+              <p class="flow-setting-item-title">抄送人</p>
+              <FlowNodeApproval :groups="node.approverGroup" :node="node" title="抄送人" />
+            </div>
+            <div v-if="noticeType.includes(3) && emailExt.includes(2)" class="flow-setting-item">
+              <p class="flow-setting-item-title">密送人</p>
+              <FlowNodeApproval :groups="node.approverGroup" :node="node" title="密送人" />
+            </div>
           </div>
-          <a-select :size="size" style="width: 100%;margin-bottom: 20px;" placeholder="请选择模板"></a-select>
-          <a-textarea v-model="noticeContext" :size="size" :rows="4" placeholder="通知内容" />
-        </div>
-        <div class="flow-setting-item">
-          <p class="flow-setting-item-title">发送通知人</p>
-          <FlowNodeApproval :groups="node.approverGroup" :node="node" title="通知人" />
-        </div>
-        <div v-if="noticeType.includes(2)" class="flow-setting-item">
-          <p class="flow-setting-item-title">外部手机号</p>
-          <a-button type="link" icon="plus" block>
-            添加手机号
-          </a-button>
-        </div>
-        <div v-if="noticeType.includes(3)" class="flow-setting-item">
-          <p class="flow-setting-item-title">外部邮箱账号</p>
-          <a-button type="link" icon="plus" block>
-            添加邮箱
-          </a-button>
-        </div>
-        <div v-if="noticeType.includes(3)" class="flow-setting-item">
-          <a-checkbox-group v-model="emailExt">
-            <a-row :gutter="[12, 12]">
-              <a-col :span="12" v-for="(item, i) in emailItems" :key="i">
-                <a-checkbox :value="item.value">{{ item.name }}</a-checkbox>
-              </a-col>
-            </a-row>
-          </a-checkbox-group>
-        </div>
-        <div v-if="noticeType.includes(3) && emailExt.includes(1)" class="flow-setting-item">
-          <p class="flow-setting-item-title">抄送人</p>
-          <FlowNodeApproval :groups="node.approverGroup" :node="node" title="抄送人" />
-        </div>
-        <div v-if="noticeType.includes(3) && emailExt.includes(2)" class="flow-setting-item">
-          <p class="flow-setting-item-title">密送人</p>
-          <FlowNodeApproval :groups="node.approverGroup" :node="node" title="密送人" />
-        </div>
-      </div>
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="内容设置">
+          <div class="flow-setting-content">
+            <div class="flow-setting-item">
+              <p class="flow-setting-item-title">通知内容</p>
+              <div class="tpl-flex-box">
+                <div>选择已审核模板</div>
+                <a-button type="link" icon="plus-circle">
+                  创建新模板
+                </a-button>
+              </div>
+              <a-select :size="size" style="width: 100%;margin-bottom: 20px;" placeholder="请选择模板"></a-select>
+              <a-textarea v-model="noticeContext" :size="size" :rows="4" placeholder="通知内容" />
+            </div>
+          </div>
+        </a-tab-pane>
+        <a-tab-pane key="3" tab="高级设置"></a-tab-pane>
+      </a-tabs>
     </div>
     <FlowDrawerFooter @close="onClose" />
   </a-drawer>
